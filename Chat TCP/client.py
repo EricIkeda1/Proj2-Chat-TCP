862b9029f3b7c660459157963808fc76de83a303
ikedayuji <ikedayuji.2002@gmail.com>
2024-08-20 13:03:17 -0300

    cifra de cesar adicionado

--- Chat TCP/client.py
+++ Chat TCP/client.py
@@ -1,49 +1,74 @@
 import socket
 import threading
 
-print("Tarefa 2")
+def caesar(data, key, mode):
+    alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    new_data = ''
+    for c in data:
+        index = alphabet.find(c)
+        if index == -1:
+            new_data += c
+        else:
+            new_index = index + key if mode == 'encrypt' else index - key
+            new_index = new_index % len(alphabet)
+            new_data += alphabet[new_index:new_index+1]
+    return new_data
 
+def encrypt_message(message, cipher_type, key):
+    if cipher_type == '1':  # Caesar Cipher
+        return caesar(message, key, 'encrypt')
+    # Add other cipher implementations here
+    return message
+
+def decrypt_message(message, cipher_type, key):
+    if cipher_type == '1':  # Caesar Cipher
+        return caesar(message, key, 'decrypt')
+    # Add other cipher implementations here
+    return message
+
+print("Tarefa 2")
 print("Atividade em Dupla: Implementação de Cifras Clássicas em um Chat TCP")
 
 # Conectando ao servidor
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Solicitar ao usuário que digite o número do servidor e a porta
-server_ip = input("Digite o endereço IP do servidor exemplo '127.0.0.1': ")
+server_ip = input("Digite o endereço IP do servidor, exemplo '127.0.0.1': ")
 server_port = input("Digite a porta do servidor, exemplo '55555': ")
 
-# Conectar ao servidor usando os valores fornecidos pelo usuário
-client.connect((server_ip, int(server_port)))
+try:
+    # Conectar ao servidor usando os valores fornecidos pelo usuário
+    client.connect((server_ip, int(server_port)))
+except socket.error as e:
+    print(f"Erro ao conectar ao servidor: {e}")
+    exit()
 
 # Escolhendo um apelido
 nickname = input("Escolha seu apelido:\n ")
 
+print("1. Cifra de César")
+print("2. Substituição Monoalfabética")
+print("3. Cifra de Playfair")
+print("4. Cifra de Vigenère")
 
-print("1.Cifra de César")
-print("2.Substituição Monoalfabética")
-print("3.Cifra de Playfair")
-print("4.Cifra de Vigenère")
-        
 # Usuário vai digitar e ser lido uma das opções
-cifra = input("Digite uma das opções, cifra de criptografia a ser utilizada: \n") 
-        
+cifra = input("Digite uma das opções, cifra de criptografia a ser utilizada: \n")
 
-#Em seguida, o usuário deverá fornecer a chave previamente combinada com seu parceiro.
-chave = input("Digite o valor da chave entre 1 a 4, para codificar e decodificar a mensagem: \n") 
+# Em seguida, o usuário deverá fornecer a chave previamente combinada com seu parceiro.
+chave = int(input("Digite o valor da chave entre 1 a 4, para codificar e decodificar a mensagem: \n"))
 
-
-# Agora o cliente está conectado ao servidor usando os valores digitados pelo usuário
 # Função para ouvir o servidor e enviar o apelido
 def receive():
     while True:
         try:
             # Receber mensagem do servidor
-            # Se a mensagem for 'NICK', enviar o apelido
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
             else:
-                print(message)
+                # Decodificar a mensagem recebida
+                decoded_message = decrypt_message(message, cifra, chave)
+                print(decoded_message)
         except:
             # Fechar conexão em caso de erro
             print("Ocorreu um erro!")
@@ -54,7 +79,9 @@ def receive():
 def write():
     while True:
         message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        # Codificar a mensagem antes de enviar
+        encoded_message = encrypt_message(message, cifra, chave)
+        client.send(encoded_message.encode('utf-8'))
         
 # Iniciando threads para ouvir e escrever
 receive_thread = threading.Thread(target=receive)
--- Chat TCP/server.py
+++ Chat TCP/server.py
@@ -1,6 +1,31 @@
 import socket
 import threading
 
+def caesar(data, key, mode):
+    alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    new_data = ''
+    for c in data:
+        index = alphabet.find(c)
+        if index == -1:
+            new_data += c
+        else:
+            new_index = index + key if mode == 'encrypt' else index - key
+            new_index = new_index % len(alphabet)
+            new_data += alphabet[new_index:new_index+1]
+    return new_data
+
+def encrypt_message(message, cipher_type, key):
+    if cipher_type == '1':  # Caesar Cipher
+        return caesar(message, key, 'encrypt')
+    # Add other cipher implementations here
+    return message
+
+def decrypt_message(message, cipher_type, key):
+    if cipher_type == '1':  # Caesar Cipher
+        return caesar(message, key, 'decrypt')
+    # Add other cipher implementations here
+    return message
+
 # Dados da conexão
 host = '127.0.0.1'
 port = 55555
@@ -17,43 +42,51 @@ nicknames = []
 # Função para enviar mensagens para todos os clientes conectados
 def broadcast(message):
     for client in clients:
-        client.send(message)
-        
+        try:
+            client.send(message)
+        except:
+            # Remove client from list if there's an error
+            clients.remove(client)
+            client.close()
+
 # Função para lidar com mensagens dos clientes
 def handle(client):
+    cipher_type = '1'  # Default to Caesar Cipher
+    key = 1            # Default key
+    
     while True:
         try:
             # Recebendo mensagens e transmitindo para todos os clientes
-            message = client.recv(1024)
-            broadcast(message)
+            message = client.recv(1024).decode('utf-8')
+            decrypted_message = decrypt_message(message, cipher_type, key)
+            broadcast(decrypted_message.encode('utf-8'))
         except:
             # Removendo e fechando clientes desconectados
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast('{} saiu!'.format(nickname).encode('ascii'))
+            broadcast(f'{nickname} saiu!'.encode('utf-8'))
             nicknames.remove(nickname)
             break
-        
+
 # Função para receber conexões
 def receive():
     while True:
         # Aceitando conexão
         client, address = server.accept()
-        print("Conectado com {}".format(str(address)))
+        print(f"Conectado com {str(address)}")
 
         # Solicitando e armazenando apelido
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
 
         # Imprimindo e transmitindo apelido
-        print("Apelido é {}".format(nickname))
-        broadcast("{} entrou!".format(nickname).encode('ascii'))
-        client.send('Conectado ao servidor, Opções!'.encode('ascii'))
-
+        print(f"Apelido é {nickname}")
+        broadcast(f"{nickname} entrou!".encode('utf-8'))
+        client.send('Conectado ao servidor, Opções!'.encode('utf-8'))
 
         # Iniciando thread para lidar com o cliente
         thread = threading.Thread(target=handle, args=(client,))
