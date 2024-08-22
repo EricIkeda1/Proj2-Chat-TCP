243a951af876e37cacf2d6b49bc85bc83959e76a
ikedayuji <ikedayuji.2002@gmail.com>
2024-08-22 13:10:50 -0300

    playfair

--- Chat TCP/client.py
+++ Chat TCP/client.py
@@ -7,10 +7,6 @@ class SimpleSubstitution:
         self.decrypting = False
 
     def cipher_alphabet(self, password):
-        ''' (str) -> str
-        Retorna um alfabeto cifrado iniciado com
-        o texto da palavra chave password
-        '''
         c_alphabet = []
         password = password.upper()
         for ch in password:
@@ -24,10 +20,6 @@ class SimpleSubstitution:
         return ''.join(c_alphabet)
 
     def encrypt(self, plaintext, password):
-        '''(str, str) -> str
-        Retorna o texto plano cifrado com a cifra de
-        substituicao com a palavra chave password
-        '''
         txt = ''
         p_alphabet = self.p_alphabet
         text = plaintext.replace(' ', '').upper()
@@ -40,10 +32,6 @@ class SimpleSubstitution:
         return txt
 
     def decrypt(self, ciphertext, password):
-        '''(str, str) -> str
-        Retorna o texto cifrado decifrado com a cifra de
-        substituicao com a palavra chave password
-        '''
         self.decrypting = True
         return self.encrypt(ciphertext, password)
 
@@ -66,6 +54,8 @@ def encrypt_message(message, cipher_type, key, password=None):
     elif cipher_type == '2':  # Substitution Cipher
         substitution = SimpleSubstitution()
         return substitution.encrypt(message, password)
+    elif cipher_type == '3':  # Playfair Cipher
+        return playfair_cipher(message, password, 'encrypt')
     # Add other cipher implementations here
     return message
 
@@ -75,27 +65,73 @@ def decrypt_message(message, cipher_type, key, password=None):
     elif cipher_type == '2':  # Substitution Cipher
         substitution = SimpleSubstitution()
         return substitution.decrypt(message, password)
+    elif cipher_type == '3':  # Playfair Cipher
+        return playfair_cipher(message, password, 'decrypt')
     # Add other cipher implementations here
     return message
+
+def playfair_cipher(plaintext, key, mode):
+    alphabet = 'abcdefghiklmnopqrstuvwxyz'
+    key = key.lower().replace(' ', '').replace('j', 'i')
+    key_square = ''
+    for letter in key + alphabet:
+        if letter not in key_square:
+            key_square += letter
+    plaintext = plaintext.lower().replace(' ', '').replace('j', 'i')
+    if len(plaintext) % 2 == 1:
+        plaintext += 'x'
+    digraphs = [plaintext[i:i+2] for i in range(0, len(plaintext), 2)]
+
+    def encrypt(digraph):
+        a, b = digraph
+        row_a, col_a = divmod(key_square.index(a), 5)
+        row_b, col_b = divmod(key_square.index(b), 5)
+        if row_a == row_b:
+            col_a = (col_a + 1) % 5
+            col_b = (col_b + 1) % 5
+        elif col_a == col_b:
+            row_a = (row_a + 1) % 5
+            row_b = (row_b + 1) % 5
+        else:
+            col_a, col_b = col_b, col_a
+        return key_square[row_a*5+col_a] + key_square[row_b*5+col_b]
+
+    def decrypt(digraph):
+        a, b = digraph
+        row_a, col_a = divmod(key_square.index(a), 5)
+        row_b, col_b = divmod(key_square.index(b), 5)
+        if row_a == row_b:
+            col_a = (col_a - 1) % 5
+            col_b = (col_b - 1) % 5
+        elif col_a == col_b:
+            row_a = (row_a - 1) % 5
+            row_b = (row_b - 1) % 5
+        else:
+            col_a, col_b = col_b, col_a
+        return key_square[row_a*5+col_a] + key_square[row_b*5+col_b]
+
+    result = ''
+    for digraph in digraphs:
+        if mode == 'encrypt':
+            result += encrypt(digraph)
+        elif mode == 'decrypt':
+            result += decrypt(digraph)
+
+    return result
 
 print("Tarefa 2")
 print("Atividade em Dupla: Implementação de Cifras Clássicas em um Chat TCP")
 
-# Conectando ao servidor
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Solicitar ao usuário que digite o número do servidor e a porta
 server_ip = input("Digite o endereço IP do servidor, exemplo '127.0.0.1': ")
 server_port = input("Digite a porta do servidor, exemplo '55555': ")
 
 try:
-    # Conectar ao servidor usando os valores fornecidos pelo usuário
     client.connect((server_ip, int(server_port)))
 except socket.error as e:
     print(f"Erro ao conectar ao servidor: {e}")
     exit()
 
-# Escolhendo um apelido
 nickname = input("Escolha seu apelido:\n ")
 
 print("1. Cifra de César")
@@ -103,47 +139,41 @@ print("2. Substituição Monoalfabética")
 print("3. Cifra de Playfair")
 print("4. Cifra de Vigenère")
 
-# Usuário vai digitar e ser lido uma das opções
 cifra = input("Digite uma das opções, cifra de criptografia a ser utilizada: \n")
 
-# Função para determinar o tipo de cifra e chave
 if cifra == '1':
     chave = int(input("Digite o valor da chave entre 1 a 25 para cifrar e decifrar a mensagem: \n"))
     password = None
 elif cifra == '2':
     password = input("Digite a palavra-chave para a cifra de substituição: \n")
     chave = None
+elif cifra == '3':
+    password = input("Digite a palavra-chave para a cifra de Playfair: \n")
+    chave = None
 else:
     print("Opção de cifra inválida")
     exit()
 
-# Função para ouvir o servidor e enviar o apelido
 def receive():
     while True:
         try:
-            # Receber mensagem do servidor
             message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
                 client.send(nickname.encode('utf-8'))
             else:
-                # Decodificar a mensagem recebida
                 decoded_message = decrypt_message(message, cifra, chave, password)
                 print(decoded_message)
         except:
-            # Fechar conexão em caso de erro
             print("Ocorreu um erro!")
             client.close()
             break
 
-# Função para enviar mensagens ao servidor
 def write():
     while True:
         message = '{}: {}'.format(nickname, input(''))
-        # Codificar a mensagem antes de enviar
         encoded_message = encrypt_message(message, cifra, chave, password)
         client.send(encoded_message.encode('utf-8'))
 
-# Iniciando threads para ouvir e escrever
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
